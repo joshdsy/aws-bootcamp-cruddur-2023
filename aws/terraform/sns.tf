61baf97986c10a6385108b51892bc3d3f61aab82
joshdsy <joshdsy@gmail.com>
2023-02-19 03:32:30 +0000

    removed terraform and py to create billing alarms

--- aws/terraform/sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "my_sns_topic" {
-  name = "my_sns_topic"
-}
-
-resource "aws_sns_topic_subscription" "my_sns_subscription" {
-  topic_arn = aws_sns_topic.my_sns_topic.arn
-  protocol = "email"
-  endpoint = "johndoe@example.com"
-}
